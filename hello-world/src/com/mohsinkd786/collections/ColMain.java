11f53a65203d90fa9e832725f965a25f95f892be
techmomo <mohsin.khursheed@techjini.com>
2021-07-09 10:22:11 +0530

    map

---
+++ hello-world/src/com/mohsinkd786/collections/ColMain.java
@@ -0,0 +1,8 @@
+package com.mohsinkd786.collections;
+
+public class ColMain {
+    public static void main(String[] args) {
+        UserMap userMap = new UserMap();
+        userMap.execute();
+    }
+}
---
+++ hello-world/src/com/mohsinkd786/collections/UserMap.java
@@ -0,0 +1,121 @@
+package com.mohsinkd786.collections;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import static java.lang.System.out;
+public class UserMap {
+
+    public void execute(){
+        // Map - HashMap , TreeMap, LinkedHashMap, ConcurrentHashMap
+        // Dictionary - HashTable
+        // obselete class now
+        //Dictionary dictionary = new Hashtable();
+        Hashtable<String,String> hashtable = new Hashtable();
+
+        String str = "";
+        String str1 = new String("");
+        str1.intern();
+
+        // uniqueness for keys
+        // no null keys & values
+        // no duplication allowed
+        hashtable.put("1","on");
+
+        hashtable.put("2","off");
+        // not permitted
+        //hashtable.put(null,"");
+        //hashtable.put("#",null);
+
+        // new object();
+        // new object();
+
+        // hashtable working
+
+        // hashCode
+        // equals
+
+        //String
+        // uses a term referred as hashcode
+
+
+        String oldVal = hashtable.put("1","pause");
+
+        out.println("old value "+oldVal);
+
+        hashtable.entrySet().forEach(entry -> out.println(entry.getKey() +"---"+entry.getValue()));
+
+
+        HashMap<UserKey,String> map = new HashMap<>();
+
+//        map.put("1","one");
+//        map.put("1","1");
+//        map.put("2","two");
+
+        UserKey usr1 = new UserKey("1","User1");
+        UserKey usr2 = new UserKey("1","User2");
+        UserKey usr3 = new UserKey("2","User2");
+        UserKey usr4 = new UserKey("3","User3");
+
+        int hashCode1 = usr1.hashCode();
+        int hashCode2 = usr2.hashCode();
+        int hashCode3 = usr3.hashCode();
+        int hashCode4 = usr4.hashCode();
+
+        map.put(usr1,"Hola");
+        map.put(usr2,"Hey");
+        map.put(usr3,"Hello");
+        map.put(usr4,"Wassup");
+
+
+        map.entrySet().forEach(e-> out.println(e.getKey() +" -- " +e.getValue()));
+
+        String val = map.get(new UserKey("1","User1"));
+
+        out.println("VAL : "+val);
+        out.println("HashCode  : "+hashCode1 +",  "+hashCode2 + ", "+hashCode3 + ", "+hashCode4);
+        // allows one null key
+        // allows multiple null values
+        // is not thread safe
+
+    }
+}
+
+
+class UserKey{
+    String id;
+    String name;
+
+    public UserKey(String id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(this.getClass() != obj.getClass()){
+            return false;
+        }
+        UserKey userKey = (UserKey)obj;
+
+        return this.id.equals(userKey.id) && this.name.equals(userKey.name);
+    }
+
+    @Override
+    public String toString() {
+        return this.id +" "+this.name;
+    }
+}
