04a0206c5c4b77b94d63e037514aa0cde68c04b7
techmomo <mohsin.khursheed@techjini.com>
2021-07-13 15:37:19 +0530

    wait & notify

---
+++ hello-world/src/com/mohsinkd786/threads/NotifierMainClass.java
@@ -0,0 +1,117 @@
+package com.mohsinkd786.threads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotifierMainClass {
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        int size = 10;
+
+        Producer producer = new Producer(integers,size);
+        // create the thread
+        Thread t1 = new Thread(producer,"Producer");
+        Consumer consumer = new Consumer(integers);
+        // create the Thread
+        Thread consumer1 = new Thread(consumer,"Consumer1");
+        Thread consumer2 = new Thread(consumer,"Consumer2");
+        Thread consumer3 = new Thread(consumer,"Consumer3");
+        consumer1.setPriority(10);
+        consumer2.setPriority(7);
+        consumer3.setPriority(2);
+
+        // start the thread
+        t1.start();
+        //
+        consumer1.start();
+        //
+        consumer2.start();
+        //
+        consumer3.start();
+    }
+}
+
+class Producer implements Runnable {
+
+    private List<Integer> nums;
+    private int CAPACITY;
+
+    public Producer(List<Integer> nums, int size){
+        this.nums = nums;
+        this.CAPACITY = size;
+    }
+
+    @Override
+    public void run() {
+
+        // call the producer
+
+        int i = 0;
+        while(true){
+            try{
+                produce(i++);
+            }catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    void produce(int i) throws InterruptedException {
+
+        synchronized (nums){
+            while(nums.size() == CAPACITY){
+                System.out.println("List is full "+Thread.currentThread().getName() + " size is : "+ nums.size());
+                nums.wait();
+            }
+            Thread.sleep(1000);
+            nums.add(i);
+            System.out.println("Produced:: "+i);
+            nums.notifyAll();
+        }
+    }
+}
+
+class Consumer implements Runnable{
+
+    private List<Integer> nums;
+
+    public Consumer(List<Integer> nums){
+        this.nums = nums;
+    }
+
+    void consume() throws InterruptedException{
+
+        synchronized (nums){
+            while(nums.isEmpty()){
+                System.out.println("List is empty "+Thread.currentThread().getName() + " is waiting, size is  "+nums.size());
+                nums.wait();
+            }
+            Thread.sleep(500);
+            // consumer-1 -- waiting / blocking
+            // consumer-2 -- waiting /
+            // consumer-3 -- running
+            // allow to apply the lock - on the object
+
+            // monitor in case of threads - nums
+
+            // always remove from first index in case the value exists, i.e. the list isnt empty
+            int num = (Integer)nums.remove(0);
+            System.out.println("Consumed Thread :: "+Thread.currentThread().getName() + " --- " +num);
+            nums.notifyAll();
+            //nums.notify();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        while (true){
+            try{
+                consume();
+            }catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+}
