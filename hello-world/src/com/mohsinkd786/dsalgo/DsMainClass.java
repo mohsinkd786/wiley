57cec93beb6e6d9907de969cd951af02c38c5dc6
techmomo <mohsin.khursheed@techjini.com>
2021-07-16 15:33:19 +0530

    Intro

---
+++ hello-world/src/com/mohsinkd786/dsalgo/DsMainClass.java
@@ -0,0 +1,81 @@
+package com.mohsinkd786.dsalgo;
+
+public class DsMainClass {
+    public static void main(String[] args) {
+
+        //
+        Integer[] ints = new Integer[2];
+        ints[0] = 1;
+
+        ints[0] = 2;
+
+        // Linked List
+        // Head -> [1,[ref to node 2]]
+        //                      --> [2,[ref to node 3]]
+        //                                    --> [3,[ref to node 1]/empty]--> null / nothing
+
+        // Double ended list
+        // Head -> [null,1,[ref to node 2]]
+        //                      --> [[ref to 1],2,[ref to node 3]]
+        //                                    --> [[ref to 2],3,[ref to node 1]/empty]--> null / nothing
+
+        // Circular
+        // Head -> [1,[ref to node 2]]
+        //                      --> [2,[ref to node 3]]
+        //                                    --> [3,[ref to node 1]]-->  / nothing
+
+
+        // List
+
+
+        // Insertion  - O(1)
+        // Search + Insertion = O(n)
+        //
+
+        // 1
+        // N
+        // O()
+
+
+        // Performance Metrics
+        /**
+         *  Space Complexity
+         *
+         *
+         *  Time Complexity
+         *
+         *
+         *  Insertion - how much time does a linked list take to insert a new value
+         *  Search - how much time does a DS take to search for a record
+         *  Deletion - how much time does it need to identify & delete a record
+         *  sort -
+         *
+         *
+         *
+         *
+         *  Asymptotic Notations :
+         *
+         *  1. Big O -> worst case -
+         *  2. Omega -> best case -
+         *  3. Theta -> average case -
+         *
+         * */
+
+    }
+}
+
+/**
+ *  Data Structures
+ *
+ *  1. Linear
+ *      a. Arrays :
+ *      b. LinkedList:
+ *      c. Stack
+ *      d. Queue
+ *  2. Hierarchical / Non- Linear
+ *      a. Tree
+ *      b. Graph
+ *      c. Heap
+ *
+ *
+ * */
